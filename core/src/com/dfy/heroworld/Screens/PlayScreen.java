44ba30a5ef99d37ae119d45cdee0c4b46e074556
_iDong <dd_dong.bk@hotmail.com>
2016-11-27 13:45:49 +0700

    test

--- core/src/com/dfy/heroworld/Screens/PlayScreen.java
+++ core/src/com/dfy/heroworld/Screens/PlayScreen.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.dfy.heroworld.HeroWorld;
@@ -19,8 +22,11 @@ public class PlayScreen implements Screen {
 
     private OrthographicCamera gamecam;
     private Viewport gamePort;
-
     private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(HeroWorld game) {
         this.game = game;
@@ -28,18 +34,36 @@ public class PlayScreen implements Screen {
         gamePort = new FitViewport(400, 200,gamecam);
         hud = new Hud(game.batch);
 
+        //mapLoader = new TmxMapLoader();
+       // map = mapLoader.load("level1.tmx");
+        //renderer = new OrthogonalTiledMapRenderer(map);
+        //gamecam.position.set(gamePort.getScreenWidth() / 2 , gamePort.getScreenHeight() / 2 ,0);
+
+
     }
 
     @Override
     public void show() {
 
     }
+ /*   public void handleinput(float dt){
+        if(Gdx.input.isTouched()){
+            gamecam.position.x += 100 * dt;
+        }
+    }
+    public void update(float dt){
+        handleinput(dt);
 
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+*/
     @Override
     public void render(float delta) {
+      //  update(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+//        renderer.render();
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
@@ -47,7 +71,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
