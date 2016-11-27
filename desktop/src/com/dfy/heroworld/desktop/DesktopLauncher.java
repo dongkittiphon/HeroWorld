1363b3ebf13196a2c0b09712f812967764061922
_iDong <dd_dong.bk@hotmail.com>
2016-11-27 10:56:35 +0700

    setting gdx

---
+++ desktop/src/com/dfy/heroworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dfy.heroworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dfy.heroworld.heroWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new heroWorld(), config);
+	}
+}
